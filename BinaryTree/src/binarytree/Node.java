1f4fa0a8dbb03939760a19fc2058198fa0ee2c35
Wewen <Wewen@Wewen-PC>
2019-02-27 00:04:53 +0700

    Version 0.1

---
+++ BinaryTree/src/binarytree/Node.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarytree;
+
+/**
+ *
+ * @author Wewen
+ */
+public class Node {
+
+    private int iData;
+    private Node left;
+    private Node right;
+    private Node parent;
+
+    public Node(int value) {
+        this.iData = value;
+        left = null;
+        right = null;
+    }
+
+    public int getiData() {
+        return iData;
+    }
+
+    public void setiData(int iData) {
+        this.iData = iData;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+}
